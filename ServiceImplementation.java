76ad3570a536000b14e36321930ac231191dbb7f
Archana-Vijayakumar <varchanaa@gmail.com>
2017-07-13 15:39:06 +0530

    POST and PUT call committed

--- ServiceImplementation.java
+++ ServiceImplementation.java
@@ -1,4 +1,6 @@
 import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
 
 import com.google.gson.Gson;
 import java.util.ArrayList;
@@ -9,7 +11,7 @@ import java.util.List;
  */
 public class ServiceImplementation {
 
-
+    List<ContactAddressModel> contactAddressModelList = new ArrayList<>();
     public ValueModel setValueModel(String s1,String s2) {
 
       ValueModel valueModel = new ValueModel();
@@ -27,8 +29,11 @@ public class ServiceImplementation {
 
     public ContactAddressModel setContactAddressModel(String s, List<AttributesModel> attributesModels){
       ContactAddressModel contactAddressModel = new ContactAddressModel();
-      contactAddressModel.setContactKey("6dcb271e-06db-4100-bebb-b145469cd4d1");
+      contactAddressModel.setContactKey(s);
       contactAddressModel.setAttributes(attributesModels);
+
+      contactAddressModelList.add(contactAddressModel);
+
       return contactAddressModel;
     }
     public ContactAddressModel setValues(){
@@ -58,5 +63,44 @@ public class ServiceImplementation {
       {
         return new Gson().toJson(serviceImplementation.setValues());
       });
+
+      post("/createDetail", (request, response) -> {
+        ContactAddressModel newModel = new Gson().fromJson(request.body(), ContactAddressModel.class);
+        return new Gson().toJson(serviceImplementation.createValue(newModel));
+      });
+
+      put("/updateDetail", (request, response) ->{
+        ContactAddressModel newModel = new Gson().fromJson(request.body(), ContactAddressModel.class);
+        return  new Gson().toJson(serviceImplementation.updateDetail(newModel));
+      });
+    }
+
+    public ContactAddressModel createValue(ContactAddressModel newModel){
+      ContactAddressModel createdModel = new ContactAddressModel();
+      createdModel.setContactKey(newModel.getContactKey());
+
+      List<AttributesModel> attributesModelList = new ArrayList<>();
+      List<ValueModel> valueModelList = new ArrayList<>();
+
+      for(int i = 0; i < newModel.getAttributes().size(); i++){
+        AttributesModel attributesModel = newModel.getAttributes().get(i);
+        ItemModel itemModel = attributesModel.getItems();
+        for(int j=0; j < itemModel.getValues().size(); j++){
+          ValueModel valueModel = new ValueModel();
+          valueModel.setName(itemModel.getValues().get(j).getName());
+          valueModel.setValue(itemModel.getValues().get(j).getValue());
+          valueModelList.add(valueModel);
+        }
+        itemModel.setValues(valueModelList);
+        attributesModelList.add(attributesModel);
+      }
+      createdModel.setAttributes(attributesModelList);
+      System.out.print(new Gson().toJson(createdModel));
+      contactAddressModelList.add(createdModel);
+      return createdModel;
+    }
+
+    public ContactAddressModel updateDetail(ContactAddressModel model){
+        return model;
     }
 }
